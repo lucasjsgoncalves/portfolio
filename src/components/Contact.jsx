4a77a21bb5e9ababf22d202cb20f7a4f3f6f6349
Lucas Gonçalves <113403670+lucasjsgoncalves@users.noreply.github.com>
2024-12-16 22:43:30 -0300

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,8 @@ function Contact() {
     <div className="flex flex-col mb-10 mx-auto">
       <div className="flex justify-center items-center">
         <form
-          name="contact" netlify
+         action="https://getform.io/f/lucasjsgoncalves@gmail.com"
+          method="POST"
           className="flex flex-col w-full md:w-7/12"
       
         >
